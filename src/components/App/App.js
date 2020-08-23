64dc68b3e9f2d03a3cec6771df31852f50b27117
Agnieszka Irzykowska <agnieszkairzykowska@gmail.com>
2020-08-23 20:07:58 +0200

    Add SearchResults

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import styles from './App.scss';
 import List from '../List/ListContainer';
 import PropTypes from 'prop-types';
 import Search from '../Search/SearchContainer';
+import SearchResults from '../SearchResults/SearchResultsContainer';
 
 class App extends React.Component {
 
@@ -10,20 +11,31 @@ class App extends React.Component {
     title: PropTypes.node,
     subtitle: PropTypes.node,
     lists: PropTypes.array,
+    searchString: PropTypes.node,
   }
 
   render() {
-    const {title, subtitle, lists} = this.props;
+    const {title, subtitle, lists, searchString} = this.props;
+    
+    let view;
+    if(searchString) {
+      view = (
+        <SearchResults />
+      );
+    } else {
+      view = (
+        lists.map(listData => (
+          <List key={listData.id} {...listData} />
+        ))
+      );
+    }
+    
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
         <Search />
-        {lists.map(listData => (
-          <List key={listData.id} {...listData} />
-        ))}
-        
-       
+        {view}
       </main>
     );
   }
---
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -0,0 +1,11 @@
+import {connect} from 'react-redux';
+import SearchResults from './SearchResults';
+import { getCardsForSearchResults } from '../../redux/cardsRedux';
+
+
+const mapStateToProps = (state) => ({
+  cards: getCardsForSearchResults(state),
+});
+
+
+export default connect(mapStateToProps )(SearchResults);
